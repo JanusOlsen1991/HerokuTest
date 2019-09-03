d7472bf816a06415c1bcc210aba1468d2e917ef5
Janus <janus-olsen91@hotmail.com>
2019-09-03 11:10:16 +0200

    Initial commit

---
+++ src/main/java/TestService.java
@@ -0,0 +1,35 @@
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@Path("test")
+public class TestService {
+
+    @GET
+    public String getTest(){
+        return "Hello World";
+    }
+
+    @Path("json")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getHelloJson(){
+        return new String("Hej med dig");
+    }
+
+    @Path("mysql")
+    @GET
+    public String getSQL() throws SQLException {
+
+        return "Hej 2";
+
+    }
+
+
+}
